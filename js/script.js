488282883b9adbb31b1172f10ec0dd44b9b2cce4
Rodrigo Diaz <dsrodrigo@outlook.com>
2023-07-07 22:22:57 -0300

    ultimos detalles

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let nombre= prompt("Ingresa tu nombre por favor:");
+let apellido= prompt("Ingresa tu apellido:");
+let nombreCompleto= nombre + " " + apellido;
+
+console.log("Bienvenido", nombreCompleto);
+
+
+
